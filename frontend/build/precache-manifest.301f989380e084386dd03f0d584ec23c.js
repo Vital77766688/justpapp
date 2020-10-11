f940a5ea4564f815dfbfc8489637ea93998a6ca1
Vitaliy Ponomaryov <vital77766688@yandex.ru>
2020-10-11 09:05:41 +0600

    to heroku 3

---
+++ frontend/build/precache-manifest.301f989380e084386dd03f0d584ec23c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a132ab5daac0cf5a2dd00d4406b6b444",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1225d0b7b4fe496634e5",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "849bd6731879575d8c3a",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "1225d0b7b4fe496634e5",
+    "url": "/static/js/2.ab0b0605.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/static/js/2.ab0b0605.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "849bd6731879575d8c3a",
+    "url": "/static/js/main.a975abbe.chunk.js"
+  },
+  {
+    "revision": "b432527bd502acbb26d6",
+    "url": "/static/js/runtime-main.c2a13efd.js"
+  }
+]);
