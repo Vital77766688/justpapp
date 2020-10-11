6e02396f7b647f85556d71300171f2bf9ba56ad1
vital77766688@yandex.ru <vital77766688@yandex.ru>
2020-10-11 11:45:29 +0600

    to heroku 17

--- dj/urls.py
+++ dj/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, re_path, include
 from django.views.generic import TemplateView
 from accounts.views import ResendConfirmationEmailView
 
@@ -11,7 +11,7 @@ urlpatterns = [
 	path('auth/', include('dj_rest_auth.urls')),
     path('auth/registration/', include('dj_rest_auth.registration.urls')),
     path('admin/', admin.site.urls),
-    path('*', TemplateView.as_view(template_name='index.html')),
+    re_path('*', TemplateView.as_view(template_name='index.html')),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
